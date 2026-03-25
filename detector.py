0a812cb8de7f719d79eaa9204a86e9b66a4e9553
trojantarg21 <165901553+trojantarg21@users.noreply.github.com>
2026-03-26 01:19:28 +0530

    Update detector.py

--- detector.py
+++ detector.py
@@ -146,12 +146,3 @@ def detect_scam(text):
         "score": score,
         "reasons": list(reasons)
     }
-
-while True:
-    msg = input("\nEnter message (type 'exit' to quit): ")
-
-    if msg.lower() == "exit":
-        print("Exiting tool...")
-        break
-
-    detect_scam(msg)
